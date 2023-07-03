dd6fa063188fbdc494b5b1a1ee4ccb428d60c18a
ebaytheway <lafleurr4141@gmail.com>
2023-07-03 20:26:43 +0300

    feat:add responsive photo text

--- src/components/Photo.jsx
+++ src/components/Photo.jsx
@@ -10,8 +10,8 @@ function Photo({ img,username,name }) {
       />
       <div className="image-holder photo absolute flex flex-col justify-end text-white">
         <div className="p-5">
-          <div className="text-white font-bold">{name}</div>
-          <div className="italic text-white/50">{username}</div>
+          <div className="text-white photo-text font-bold">{name}</div>
+          <div className="italic photo-text text-white/50">{username}</div>
         </div>
       </div>
     </div>
