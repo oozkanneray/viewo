93dd6745234d47fd05dc1cf1b5e7eb15007f9852
ebaytheway <lafleurr4141@gmail.com>
2023-07-07 22:40:31 +0300

    feat:add font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,9 @@ export default {
     extend: {
       boxShadow: {
         '3xl': '0 50px 80px -35px rgba(0, 0, 0, 0.4)',
+      },
+      fontFamily:{
+        'knewave' : ['Knewave','cursive']
       }
     },
   },
